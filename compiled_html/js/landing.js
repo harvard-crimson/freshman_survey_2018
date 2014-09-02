e3c89e12e018bc7f6d41aa730e9ebed5139e7170
Sam Fishman <sfishman@college.harvard.edu>
2014-09-02 04:22:27 -0400

    gray out future sections

--- compiled_html/js/landing.js
+++ compiled_html/js/landing.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("div.card").hover(function() {
+	$("div.card:not(.card-disabled)").hover(function() {
 		$(this).stop().animate({boxShadow: '3px 3px 10px #aaaaaa'}).children('.cover').fadeIn();
 	}, function() {
 		$(this).stop().animate({boxShadow: 'none'}).children('.cover').fadeOut();
